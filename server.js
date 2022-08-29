46dd39d5e7376485ccdb2053ede9b8f2e6a13d96
rina.easterday@gmail.com <rina.easterday@gmail.com>
2022-08-28 18:12:20 -0700

    added ability to translate jpn to eng and started article search func

--- server.js
+++ server.js
@@ -1,45 +1,69 @@
 $(document).ready(function() {
     getKanji();
     $('#nextBtn').click(function() {
-        $('#translateBtn').val("translate");
+        $('#translateBtn').val("English Translation");
         getKanji();
     });
     $('#translateBtn').click(function() {
         $('#examples p').toggle();
         $("#kanjiWord #kanjiTranslation").toggle();
         var btnvalue = $(this).val();
-        $(this).val(btnvalue === "translate" ? "hide" : "translate");
+        $(this).val(btnvalue === "English Translation" ? "hide" : "English Translation");
+    });
+    $('#generateArticlesBtn').click(function() {
+        getArticles();
     })
 })
+
+function getArticles() {
+    let keyword = $('#kanjiWord #kanji').text();
+    $.ajax({
+        type: "POST",
+        url: "server.php",
+        data: {"keyword": keyword},
+    }).done(function(msg) {
+        alert(msg);
+    })
+}
 
 function getKanji() {
     $.ajax({
         type: "POST",
-        url: "server.php"
+        url: "server.php",
+        data: {"getExamples": "1"},
     }).done(function(msg) {
         kanjiObject = JSON.parse(msg);
         console.log(kanjiObject);
         $("#kanjiWord").empty();
         $("#kanjiSentence").empty();
         $('#examples').empty();
-        for (let i = 0; i < kanjiObject.length; i++) {
-            let kanji = $(`<h2 id="kanji${[i]}"></h2>`).text(kanjiObject[i]['kanji']);
-            let sentence = `<h2 id="sentence${[i]}">` + kanjiObject[i]['sentence'] + '</h2>';
-            let translation = `<p id="translation${[i]}">` + kanjiObject[i]['translation'] + '</p>';
-            let kanjiExampleUsage = `<div id="example${i}">` + sentence + translation + '</div>'; 
-            if (kanji !== null) {
-                $('#kanjiWord').append(kanji);
-            }
+        let kanji = $(`<h2 id="kanji"></h2>`).text(kanjiObject['kanji']);
+        let hiragana = $(`<p id="hiragana"></p>`).text(kanjiObject['kanjiHiragana']);
+
+        if (kanji !== null) {
+            $('#kanjiWord').append(kanji);
+        }
+
+        $('#kanjiWord').append("<div id='kanjiTranslation'></div");
+        $("#kanjiWord #kanjiTranslation").append(hiragana);
+
+        for (let j = 0; j < kanjiObject['kanjiMeaning'].length; j++) {
+            let meanings = $(`<p id="meaning${[j]}"></p>`).text(kanjiObject['kanjiMeaning'][j]);
+            $("#kanjiWord #kanjiTranslation").append(meanings);
+        }
+        $("#kanjiWord #kanjiTranslation").hide();
+
+        for (let i = 0; i < kanjiObject['exSentence'].length; i++) {
+            
+            let sentence = `<h2 id="sentence${[i]}">` + kanjiObject['exSentence'][i] + '</h2>';
+            let jpTranslation = `<p id="jpTranslation${[i]}">` + kanjiObject['jpSentTranslation'][i] + '</p>';
+            let enTranslation = `<p id="enTranslation${[i]}">` + kanjiObject['enSentTranslation'][i] + '</p>';
+            let kanjiExampleUsage = `<div id="example${i}">` + sentence + jpTranslation + enTranslation + '</div>'; 
+            
             $('#examples').append(kanjiExampleUsage);
-            console.log(kanjiExampleUsage);
-            $(`#examples #translation${[i]}`).hide();
+            $(`#examples #jpTranslation${[i]}`).hide();
+            $(`#examples #enTranslation${[i]}`).hide();
            
-            $('#kanjiWord').append("<div id='kanjiTranslation'></div");
-            for (let j = 0; j < kanjiObject[i]['kanjiMeaning'].length; j++) {
-                let meanings = $(`<p id="meaning${[j]}"></p>`).text(kanjiObject[i]['kanjiMeaning'][j]);
-                $("#kanjiWord #kanjiTranslation").append(meanings);
-            }
-            $("#kanjiWord #kanjiTranslation").hide();
         }
     }); 
 }
--- server.php
+++ server.php
@@ -1,48 +1,115 @@
 <?php
-    header("Access-Control-Allow-Origin: http://[cross.domain]");
-    header("Access-Control-Allow-Headers: access");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
-    header("Access-Control-Allow-Credentials: true");
+    if (isset($_POST['getExamples'])) {  
+        header("Access-Control-Allow-Origin: http://[cross.domain]");
+        header("Access-Control-Allow-Headers: access");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+        header("Access-Control-Allow-Credentials: true");
 
-    class kanjiObj {
-        public $kanji;
-        public $kanjiMeaning;
-        public $sentence;
-        public $translation;
-    }
-    
-    $xml = simplexml_load_file("kanji_xml/JMdict_e_examp") or die("Error: cannot create object");
-    if($xml === FALSE) {
-        echo "error";
-    }
-    else {
-        // function that returns a random kanji + sentence + translation from xml file
-        while (1) {
-            $i = rand(0, count($xml->entry));
-            if ($xml->entry[$i]->sense->example)  {
-                $kanjiExamples = array();
-                for ($j = 0; $j < count($xml->entry[$i]->sense); ++$j) {
-                    if ($xml->entry[$i]->sense[$j]->example) {
-                        $kanjiObject = new kanjiObj();
-                        if ($j === 0) {
-                            $kanjiObject->kanji = (string)$xml->entry[$i]->sense[$j]->example->ex_text;
+        class kanjiObj {
+            public $kanji;
+            public $kanjiHiragana;
+            public $kanjiMeaning;
+            public $exSentence;
+            public $jpSentTranslation;
+            public $enSentTranslation;
+        }
+
+        function getHiragana($kanji) {
+            $apiID = "xxx";
+            $url = "https://labs.goo.ne.jp/api/hiragana";
+            $data = array("app_id"=> $apiID, "sentence" => (string)$kanji, "output_type" => "hiragana");
+            $data = http_build_query($data);
+
+            $options = array(
+                'http' => array(
+                    'method' => 'POST',
+                    'header' => "Content-type: application/x-www-form-urlencoded",
+                    'content' => $data
+                )
+            );
+            $context = stream_context_create($options);
+            $result = @file_get_contents($url, false, $context);
+            if ($result === FALSE) {
+                return 0;
+            }
+            $result = json_decode($result);
+            return $result->converted;
+        }
+        
+        $xml = simplexml_load_file("kanji_xml/JMdict_e_examp") or die("Error: cannot create object");
+        if ($xml === FALSE) {
+            echo "error";
+        }
+        else {
+            // function that returns a random kanji + sentence + translation from xml file
+            while (1) {
+                $i = rand(0, count($xml->entry));
+                if ($xml->entry[$i]->sense->example) {
+                    $send = 0;
+                    $kanjiObject = new kanjiObj();
+                    $pattern = "'/^[^ぁ-んァ-ン]+/i'";
+
+                    $kanjiObject->kanji = (string)$xml->entry[$i]->sense->example->ex_text;
+                    if (((bool)preg_match('/^[^ぁ-んァ-ン]+/i', (string)$xml->entry[$i]->sense->example->ex_text)) === FALSE) {
+                        $send = 1;
+                    }
+                    // get hiragana for kanji
+                    $kanjiHiragana = getHiragana($kanjiObject->kanji);
+                    if ($kanjiHiragana === 0) {
+                        $send = 1;
+                    }
+                    else {
+                      $kanjiObject->kanjiHiragana = (string)$kanjiHiragana; 
+                    } 
+                    
+                    $meanings = array();
+                    foreach ($xml->entry[$i]->sense->gloss as $meaning) {
+                        $meanings[] = (string)$meaning;
+                    };
+                    $kanjiObject->kanjiMeaning = $meanings; 
+
+                    // iterate through every example sentence
+                    $sentences = array();
+                    $jpTranslatedSentences = array();
+                    $enTranslatedSentences = array();
+                    for ($j = 0; $j < count($xml->entry[$i]->sense->example); $j++) {
+                        if ($xml->entry[$i]->sense->example[$j]) {
+                            $jpSentence = $xml->entry[$i]->sense->example[$j]->ex_sent[0];
+                            $sentences[$j] = (string)$jpSentence; 
+                            $jpTranslatedSentence = getHiragana($jpSentence);
+                            if ($jpTranslatedSentence === 0) {
+                                $send = 1;
+                            }
+                            else {
+                               $jpTranslatedSentences[$j] = (string)$jpTranslatedSentence; 
+                            } 
+                            $enTranslatedSentences[$j] = (string)$xml->entry[$i]->sense->example[$j]->ex_sent[1]; 
                         }
-                        $meanings = array();
-                        foreach ($xml->entry[$i]->sense[$j]->gloss as $meaning) {
-                            $meanings[] = (string)$meaning;
-                        };
-                        $kanjiObject->kanjiMeaning = $meanings;
-                        $kanjiObject->sentence = (string)$xml->entry[$i]->sense[$j]->example->ex_sent[0]; 
-                        $kanjiObject->translation = (string)$xml->entry[$i]->sense[$j]->example->ex_sent[1]; 
-                        $kanjiExamples[$j] = $kanjiObject;
+                    }
+                    $kanjiObject->exSentence = $sentences;
+                    $kanjiObject->jpSentTranslation = $jpTranslatedSentences;
+                    $kanjiObject->enSentTranslation = $enTranslatedSentences;
+                    if ($send === 0) {
+                        echo json_encode($kanjiObject);
+                        break;
+                    }
+                    else {
+                        continue;
                     }
                 }
-                echo json_encode($kanjiExamples);
-                break;
+                else {
+                    continue;
+                }
             }
-            else {
-                continue;
-            }
+        }    
+    }
+    else if (isset($_POST['keyword'])) {
+        include('./simple_html_dom.php');
+        $data = $_POST['keyword'];
+        $html = file_get_html("https://www.nhk.jp/search/?q=".urlencode($data));
+        foreach ($html->find('div.idb-result') as $element) {
+            $heading = $element->find('div.search-result-crawler');
+            print_r($element);
         }
-    }    
+    }
 ?>
